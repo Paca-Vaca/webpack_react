6b6509f227270aa0e4bce66e38fa6c1eedec1ced
Paul Seleznev <argeon87@gmail.com>
2015-11-24 23:11:23 +0300

    second one

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,45 @@
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
 var path = require('path');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-  app: path.join(__dirname, 'app'),
+  app: path.join(__dirname, 'app/index.jsx'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-  entry: PATHS.app,
+const TARGET = process.env.npm_lifecycle_event;
+
+var common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
   },
+  entry: PATHS.app,
+  resolve: {
+    extensions: ['', '.js', 'jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: 'style-loader!css-loader',
+      include: PATHS.app
+    }, {
+      test: /\.jsx$/,
+      loader: 'babel',
+      include: PATHS.app
+    }]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'Kanban App'
+    })
+  ]
+};
+
+module.exports = merge(common, {
+  devtool: 'eval-source-map',
   devServer: {
     contentBase: './build',
     historyApiFallback: true,
@@ -23,17 +49,11 @@ module.exports = {
     host: process.env.HOST,
     port: process.env.PORT
   },
-  module: {
-    loaders: [{
-        test: '/\.css$/',
-        loader: 'style-loader!css-loader',
-        include: PATHS.app
-    }]
-  },
   plugins: [
-    new htmlWebpackPlugin({
-      title: 'Kanban App'
-    }),
     new webpack.HotModuleReplacementPlugin()
   ]
-};
+});
+
+if (TARGET === 'start' || !TARGET) {
+
+}
